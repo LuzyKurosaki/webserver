58fd6cac04498e07eb94d97c85dbc389eb88cd7d
LuzyKurosaki <dregion123@gmail.com>
2022-03-09 13:49:25 +0100

    Dokumentation

--- main.py
+++ main.py
@@ -31,19 +31,19 @@ def redirectIndex():
 # todolist routes
 @app.route("/list")
 def listIndex():
-    return render_template("todolist/index.html", todoLists=todo_lists)
+    return render_template("todolist/index.html", todoLists=todo_lists) 
 
 @app.route("/todo-list/<list_id>", methods = ["GET"])
 def todoShow(list_id):
     data = {}
     for list in todo_lists:
-        if list['id'] == list_id:
+        if list['id'] == list_id: #find the searched list by id
             entries = []
             for entry in todos:
-                if entry['list_id'] == list_id:
-                    for user in users: 
-                        if user['id'] == entry['user_id'] :
-                            entries.append({
+                if entry['list_id'] == list_id: # find all todo entries in this list
+                    for user in users:  #find the user belonging to the todo entry
+                        if user['id'] == entry['user_id'] : 
+                            entries.append({ #create new resource for the frontend containing the user and the todo entry
                                 'user' : user,
                                 'entry' : entry
                             })    
@@ -54,23 +54,22 @@ def todoShow(list_id):
             }
     
     if 'list' not in data:
-            return redirect(url_for('listIndex'))
-    return render_template("todolist/show.html" , data=data)
+            return redirect(url_for('listIndex')) # if the list was not found redirect to the index page
+    return render_template("todolist/show.html" , data=data) #else return the html
             
 @app.route("/todo-list/<list_id>", methods = ["DELETE"])
 def todoListDelete(list_id):
     for todo in todos:
         
-        if todo['list_id'] == list_id:
-            print('test')
+        if todo['list_id'] == list_id: #find the affected todos to remove
             todos.remove(todo)
             
     for list in todo_lists:
-        if(list['id'] == list_id):
+        if(list['id'] == list_id): #find the affected list to remove
             todo_lists.remove(list)
             break
         
-    return Response([],200)
+    return Response([],200) # return response to ajax request
 
 
 #todo routes
@@ -90,7 +89,7 @@ def todoDelete(todo_id):
 @app.route("/todo-list/store", methods = ["POST"])
 def createList ():
     id = "1"
-    if len(todos) != 0:
+    if len(todos) != 0: #if list not empty get latest entry
         lastEntry = todo_lists[-1]
         id =  str(int(lastEntry['id']) + 1)
         
@@ -106,43 +105,45 @@ def createList ():
 def createTodoEntry(list_id):
     
     id = "1"
-    if len(todos) != 0:
+    if len(todos) != 0: #if list not empty get latest entry
         todo = todos[-1]
         id =  str(int(todo['id']) + 1)
         
-    newEntry = {
+    newEntry = { #prepare new todo resource
         'id' :id,
         'name': request.form.get('name'), 
         'description': request.form.get('content'),
         'user_id': request.form.get('user'),
         'list_id': list_id
     }
-    todos.append(newEntry)
+    todos.append(newEntry) #create new todo
     
     return redirect(url_for('todoShow', list_id=list_id))
     
 @app.route("/todo/<todo_id>/edit", methods= ["GET"])
 def todoEdit(todo_id):
     selectedTodo = {}
-    for todo in todos:
+    for todo in todos: # find todo entry
         if todo['id'] == todo_id:
             selectedTodo = todo
             break
-        
+         
     data = {
         'todo': selectedTodo,
         'users': users
     }
     return render_template('todolist/edit.html', data=data)
+
 @app.route('/todo/<todo_id>/update', methods=["POST"])
 def todoUpdate(todo_id):
     for todo in todos:
-        if todo['id'] == todo_id:
+        if todo['id'] == todo_id: # find effected todo entry and rewrite all data
             todo['name'] = request.form.get('name')
             todo['user_id'] = request.form.get('user')
             todo['description'] = request.form.get('content')
             break
     return redirect(url_for('todoShow', list_id=todo['list_id']))
+
 @app.route('/user', methods=["GET"])
 def userIndex():
     return render_template('user/index.html', data=users)
@@ -150,20 +151,22 @@ def userIndex():
 @app.route("/user/store", methods=["POST"])
 def storeUser():
     id = "1"
-    if len(users) != 0:
+    if len(users) != 0: #if list not empty get latest entry
         lastEntry = users[-1]
         id =  str(int(lastEntry['id']) + 1)
-    users.append({
+    users.append({ #create new user
         'id': id,
         'name': request.form.get('name')
     })
     return redirect(url_for('userIndex'))
+
 @app.route("/user/<user_id>", methods=["GET"])
 def deleteUser(user_id):
     for user in users:
-        if user['id'] == user_id:
+        if user['id'] == user_id: # find effected user entry
             users.remove(user)
             break
     return redirect(url_for('userIndex'))
+
 if __name__ == "__main__" :
     app.run(debug=True)
